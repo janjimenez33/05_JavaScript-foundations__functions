24ce422976987bfe1b02bf7ed71fe5f47b8a248a
Jan <145783994+janjimenez33@users.noreply.github.com>
2024-12-01 15:22:18 +0100

    Initial commit

---
+++ exercise_07/isTypeOf.js
@@ -0,0 +1,11 @@
+/**
+ * #7 :: Export JS function "isTypeOf" to check if type of value matches type received as argument
+ * input: any (value)
+ * input: 'string' | 'number' | 'boolean' | 'array' (type)
+ * output: boolean
+ */
+
+/**
+ * hint 👉 array should be treated in a different way. Use Array.isArray() to check it
+ * docu 👉 https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+ */
